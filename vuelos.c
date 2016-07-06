86ee13b1706c2fb4ce44cf59126497a46f27994f
EliecerZ <eli.zambrano.r@gmail.com>
2016-07-05 22:41:09 -0400

    Update vuelos.c

--- vuelos.c
+++ vuelos.c
@@ -1,4 +1,14 @@
 #include <stdlib.h>
+#include <stdio.h> //xd
+typedef struct node {               // Estructura nodo de lista enlazada
+    int ciudad;
+    struct node *next;
+} tNodo;
+
+typedef struct lista {              // Estructura lista
+    tNodo *head;                    // Almacena la cabeza de la lista enlazada
+} tLista;
+
 typedef struct vertex {
     unsigned long ciudad;
     struct vertex *adyacente;
