e766f7fb3375d80010e8c2f39048404b04676b80
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2016-07-06 17:53:29 -0400

    agregada base tda's, actualizados structs

--- vuelos.c
+++ vuelos.c
@@ -1,14 +1,45 @@
 #include <stdlib.h>
-typedef struct vertex {
+#include <stdio.h>
+typedef struct {
     unsigned long ciudad;
-    struct vertex *adyacente;
-} vertex;
+    struct tLista *destinos;
+} tVertice;
 
 typedef struct {
-    vertex *head;
+    tVertice ciudad;
+    unsigned long nCiudades;
 } tGrafo;
 
+/* FUNCIONES LISTA ENLAZADA (ELI) */
+int insert (tLista item);
+int append (tLista item);
+void moveToStart ();
+void nextL ();
+int length ();
+tLista getValue ();
+/*******************************/
 
-int maint(){
+/* FUNCIONES GRAFO (JORGE) */
+void initGraph(tGrafo *G, int n);
+void destroyGraph (tGrafo *G);
+int nVertex (tGrafo *G);
+tVertice first (tGrafo *G, tVertice v);
+tVertice nextG (tGrafo *G, tVertice v, int i);
+void setEdge (tGrafo *G, tVertice v1, tVertice v2, int peso);
+int isEdge (tGrafo *G, tVertice v1, tVertice v2);
+int getMark (tGrafo *G, tVertice v);
+void setMark (tGrafo *G, tVertice v, int marca);
+/**************************/
+
+int main(){
+	unsigned long nCiudades, nVuelos, i;
+	scanf("%lu", &nCiudades);								// Cantidad de ciudades
+	tGrafo *vuelos = (tGrafo *)malloc(sizeof(tGrafo) * nCiudades);
+	initGraph(vuelos, nCiudades);							// Inicializar grafo
+	scanf("%lu", &nVuelos);
+	unsigned int partida, destino;
+	for (i=0; i<nVuelos; i++){
+		scanf("%du %du", &partida, &destino);
+	}
     return 1;
 }
