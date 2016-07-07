1af7da2cbebfc2f528e33087bd075bc9121440ee
EliecerZ <eli.zambrano.r@gmail.com>
2016-07-07 11:55:48 -0400

    vuelos actualizado

--- vuelos.c
+++ vuelos.c
@@ -1,36 +1,39 @@
 #include <stdlib.h>
-#include <stdio.h> //xd
+#include <stdio.h>
 
-typedef struct lista {              // Estructura lista
-    tNodo *head;                   // Almacena el vertice que irá al arreglo
-    tNodo *tail;                    //-------------------
-    tNodo *curr;                    //--------------------
-    unsigned int tamaño;             //-----------------
-    unsigned int pos                 //-------------------- posicion acutal, borrar si no lo ocupamos
-} tLista;
 
 typedef struct nodo {
     unsigned long ciudad;
     struct nodo *adyacente;
 }tNodo;
 
+
+typedef struct lista {              // Estructura lista
+    tNodo *head;                   // Almacena el vertice que irá al arreglo
+    tNodo *tail;                    //-------------------
+    tNodo *curr;                    //--------------------
+    unsigned int tamaño;             //-----------------
+    unsigned int pos;            //-------------------- posicion acutal, borrar si no lo ocupamos
+} tLista;
+
 typedef struct {
     tLista vecinos;
 } tGrafo;
 
-Lista initialize(tLista lista){//head = tail = curr = NULL;
+tLista initialize(tLista *lista){//head = tail = curr = NULL;
     lista->head = (tNodo *)malloc(sizeof(tNodo));       // tarea2 : lista.head = (tNodo *)malloc(sizeof(tNodo));  
-    (lista->head)->(lista->adyacente) = NULL;        // tarea2 lista.head->adyacente = NULL;
+    (lista->head)->adyacente = NULL;        // tarea2 lista.head->adyacente = NULL;
     lista->tamaño=0;
-    return lista;
+    return *lista;
 }
 
 
 void nextL(tLista *lista) {
-    lista->curr=(lista->curr)->adyacente;
-    (lista->pos)++ //REVISAR
+    if ((lista->curr)->adyacente != NULL) {
+        lista->curr = (lista->curr)->adyacente;
+        (lista->pos)++;//REVISAR
+    }
 }
-
 
 int insertar(tLista *lista, unsigned long elem) { //este insert es un append???
     tNodo *aux = (lista->curr)->adyacente;
@@ -56,7 +59,7 @@ int moveToStart(tLista *lista){
     
 }
 int length(tLista *lista){
-    return lista->tamaño
+    return lista->tamaño;
 }
 
 unsigned int getValue(tLista *lista, int pos){
